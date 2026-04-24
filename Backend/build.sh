bbbd8b3ebbbd290851f27891b87179b408e5739c
haritha <vadepalliharitha29@gmail.com>
2026-04-24 23:23:27 +0530

    Added script to automatically generate superuser

--- Backend/build.sh
+++ Backend/build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a superuser since Render blocks Free Tier Shell access
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+"
