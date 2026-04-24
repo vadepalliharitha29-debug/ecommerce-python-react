a2c1c42d196859fecdbfc50d74d36a84189bef69
haritha <vadepalliharitha29@gmail.com>
2026-04-24 20:10:29 +0530

    Mocked PyMySQL version for Django strict checks

--- Backend/backend/__init__.py
+++ Backend/backend/__init__.py
@@ -1,3 +1,8 @@
 import pymysql
 
 pymysql.install_as_MySQLdb()
+
+# Django strictly checks the version of MySQLdb. PyMySQL pretends to be an old version.
+# We must mock the version to convince Django 6 that it's up to date.
+import MySQLdb
+MySQLdb.version_info = (2, 2, 1, "final", 0)
